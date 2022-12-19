1d6774dd8732c1825ba8f1eb4f3a807df3285f25
Leyla <git@jan-leila.com>
2022-12-19 15:33:20 -0600

    increased access token ttl move refresh token to query body

--- src/main/kotlin/com/mcspeedrun/rng/controller/AuthenticationController.kt
+++ src/main/kotlin/com/mcspeedrun/rng/controller/AuthenticationController.kt
@@ -4,10 +4,8 @@ import com.mcspeedrun.rng.model.AuthenticationMethod
 import com.mcspeedrun.rng.model.YggdrasilRegistration
 import com.mcspeedrun.rng.model.http.http401
 import com.mcspeedrun.rng.service.AuthenticationService
-import io.micronaut.http.HttpHeaderValues.AUTHORIZATION_PREFIX_BEARER
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.Post
 import io.micronaut.security.token.jwt.render.AccessRefreshToken
 import javax.annotation.security.PermitAll
@@ -31,12 +29,8 @@ class AuthenticationController (
 
     @Post("refresh")
     fun name(
-        @Header("Authorization") token: String
+        @Body refreshToken: String
     ): AccessRefreshToken {
-        if (!token.startsWith(AUTHORIZATION_PREFIX_BEARER)) {
-            throw http401("unable to parse token")
-        }
-        val refreshToken = token.substring(AUTHORIZATION_PREFIX_BEARER.length + 1)
         return authenticationService.refreshInstance(refreshToken)
     }
 }
